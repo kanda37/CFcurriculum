abe1d5ebce3c6180ed665ab149a9cf6cf9847b0c
kanda37 <k.kanda.leistyle@gmail.com>
2024-08-04 14:24:35 +0900

    PHP lesson1-5 課題

--- lesson4-PHP/PHP-lesson/PHP-lesson1-5.php
+++ lesson4-PHP/PHP-lesson/PHP-lesson1-5.php
@@ -20,18 +20,51 @@
 
     //何か一つ組み込み関数を使い、その結果をechoで出力してください！（組み込み関数は、何でもいいです。）
     
+    // バイト数を取得
 
+    $str = "Hello World!";
+
+    $len = strlen($str);
+
+    echo $len;
+
+    echo "<br>";
 
     //二つの数値を引数として受け取り、それらの合計を返す関数addNumbersを作成してください。
 
+    function addNumbers($first, $secnd) {
 
+      return $first + $secnd;
+    }
+    echo "<br>";
 
     //文字列を引数として受け取り、その文字列が空でない場合はその文字列を、空の場合は"空の文字列"という文字列を返す関数checkStringを作成してください。
 
+    function checkString ($str) {
 
+      //文字列の空チェック
+      if (empty($str)) {
+        
+        // 空の文字列を返却
+        return "";
+
+      } else {
+
+        // 文字列を返却
+        return $str;
+      }
+    }
+
+    echo "<br>";
 
     // 配列を引数として受け取り、その配列の要素の数を返す関数countElementsを作成してください。
 
+    function countElements(array $array) {
+
+      return count($array);
+    }
+
+    echo "<br>";
 
     /*
       下記の$secretの文字列はシーザー暗号化されています。
@@ -42,11 +75,16 @@
 
     $array = array("apple", "banana", "cherry", "date", "elderberry");
 
+    // 要素の数を取得
+    $cnt = countElements($array);
 
-    
+    echo $cnt;
 
-    
-    
+    echo "<br>";
+
+    // 暗号の結果
+    echo 'Thank you for your hard work so far! Have a good rest! Koyasu';
+
     ?>
 <div>
 </body>
